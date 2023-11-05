9cc4547aa7366f3d548575db612ba58b34088dfd
Alfonso Hernandez <alfonso.hernandez@astian.org>
2023-11-05 05:51:56 -0500

    change search engine to AstianGO

--- toolkit/components/search/SearchService.sys.mjs
+++ toolkit/components/search/SearchService.sys.mjs
@@ -2287,8 +2287,8 @@ export class SearchService {
     }
 
     const engines = [
-      { webExtension: { id: "bing@search.mozilla.org",      locale: "default"}},
       { webExtension: { id: "astiango@search.mozilla.org",  locale: "default"}, default: "yes" },
+      { webExtension: { id: "bing@search.mozilla.org",      locale: "default"}},
       { webExtension: { id: "google@search.mozilla.org",    locale: "default"}},
       { webExtension: { id: "startpage@search.mozilla.org", locale: "default"}},
       { webExtension: { id: "ddg@search.mozilla.org",       locale: "default"}},
