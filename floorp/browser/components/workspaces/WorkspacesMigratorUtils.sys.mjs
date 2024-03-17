efc193b50a983cd03a6b383525b29e5734be8de5
Ponchale <alfonso.hernandez@astian.org>
2024-03-16 19:17:46 -0500

    redesign of workspaces and preparations are made for the inclusion of Astian Docs

---
+++ floorp/browser/components/workspaces/WorkspacesMigratorUtils.sys.mjs
@@ -0,0 +1,169 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export const EXPORTED_SYMBOLS = ["WorkspacesMigratorUtils"];
+
+const lazy = {};
+
+ChromeUtils.defineESModuleGetters(lazy, {
+  WorkspacesService: "resource:///modules/WorkspacesService.sys.mjs",
+  WorkspacesIdUtils: "resource:///modules/WorkspacesIdUtils.sys.mjs",
+});
+
+function getIconNameByWorkspaceName(workspaceName) {
+  const settings = JSON.parse(
+    Services.prefs.getStringPref("floorp.browser.workspace.info"),
+  );
+
+  const targetWorkspaceNumber = checkWorkspaceInfoExist(workspaceName);
+  if (
+    targetWorkspaceNumber === false ||
+    settings[targetWorkspaceNumber][workspaceName].icon == "" ||
+    settings[targetWorkspaceNumber][workspaceName].icon == undefined
+  ) {
+    return null;
+  }
+  let iconURL = settings[targetWorkspaceNumber][workspaceName].icon;
+  let removeSVG = iconURL.replace("chrome://browser/skin/workspace-icons/", "");
+  let result = removeSVG.replace(".svg", "");
+  return result;
+}
+
+function checkWorkspaceInfoExist(name) {
+  const data = JSON.parse(
+    Services.prefs.getStringPref("floorp.browser.workspace.info"),
+  );
+  for (let i = 0; i < data.length; i++) {
+    const obj = data[i];
+    const keys = Object.keys(obj);
+    const workspaceValue = keys[0];
+
+    if (workspaceValue == name) {
+      // return workspaceValue;
+      return i;
+    }
+  }
+  return false;
+}
+
+function getWorkspaceUserContextId(workspaceName) {
+  const settings = JSON.parse(
+    Services.prefs.getStringPref("floorp.browser.workspace.info"),
+  );
+  const targetWorkspaceNumber = checkWorkspaceInfoExist(workspaceName);
+  if (
+    targetWorkspaceNumber === false ||
+    settings[targetWorkspaceNumber][workspaceName].container == "" ||
+    settings[targetWorkspaceNumber][workspaceName].container == undefined
+  ) {
+    // return string "0".
+    return 0;
+  }
+  return Number(settings[targetWorkspaceNumber][workspaceName].container);
+}
+
+export const WorkspacesMigratorUtils = {
+  get IsLegacyWorkspaceEnabled() {
+    return Services.prefs.getBoolPref(
+      "floorp.browser.workspace.tab.enabled",
+      true,
+    );
+  },
+
+  get migrated() {
+    return Services.prefs.getBoolPref(
+      "floorp.browser.workspace.migrated",
+      false,
+    );
+  },
+
+  get LegacyWorkspacesData() {
+    return null;
+  },
+
+  get legacySelectedWorkspace() {
+    return Services.prefs.getStringPref("floorp.browser.workspace.current");
+  },
+
+  get legacyWorkspacesAreExist() {
+    return Services.prefs.getStringPref("floorp.browser.workspace.all") !== "";
+  },
+
+  get legacyDefaultWorkspace() {
+    return Services.prefs
+      .getStringPref("floorp.browser.workspace.all")
+      .split(",")[0];
+  },
+
+  get LegacyWorkspacesAllNamesPref() {
+    let pref = Services.prefs.getStringPref("floorp.browser.workspace.all");
+    let result = pref.split(",");
+    return result;
+  },
+
+  cleanupLegacyWorkspaces() {
+    Services.prefs.clearUserPref("floorp.browser.workspace.tabs.state");
+    Services.prefs.clearUserPref("floorp.browser.workspace.current");
+    Services.prefs.setBoolPref("floorp.browser.workspace.tab.enabled", false);
+    Services.prefs.clearUserPref("floorp.browser.workspace.all");
+  },
+
+  async importDataFromLegacyWorkspaces(tabs, windowId) {
+    if (this.migrated) {
+      return;
+    }
+
+    if (!this.legacyWorkspacesAreExist || !this.IsLegacyWorkspaceEnabled) {
+      return;
+    }
+
+    // Set migrated flag
+    Services.prefs.setBoolPref("floorp.browser.workspace.migrated", true);
+
+    const allWorkspacesName = this.LegacyWorkspacesAllNamesPref;
+
+    // Create Workspace with legacy workspace name
+    for (const name of allWorkspacesName) {
+      const workspaceId = await lazy.WorkspacesService.createWorkspace(
+        name,
+        windowId,
+        this.legacyDefaultWorkspace === name,
+      );
+
+      const workspace =
+        await lazy.WorkspacesIdUtils.getWorkspaceByIdAndWindowId(
+          workspaceId,
+          windowId,
+        );
+
+      if (this.legacySelectedWorkspace === name) {
+        await lazy.WorkspacesService.setSelectWorkspace(workspace.id, windowId);
+      }
+
+      // Workspace Container & icon
+      const userContextId = getWorkspaceUserContextId(name);
+      const iconName = getIconNameByWorkspaceName(name);
+      if (iconName || userContextId) {
+        await lazy.WorkspacesService.setWorkspaceContainerUserContextIdAndIcon(
+          workspace.id,
+          userContextId || 0,
+          iconName || "fingerprint",
+          windowId,
+        );
+      }
+
+      // Add Tabs to Workspace
+      for (const tab of tabs) {
+        if (tab.getAttribute("floorpWorkspace") === name) {
+          tab.setAttribute(
+            lazy.WorkspacesService.workspacesTabAttributionId,
+            workspace.id,
+          );
+        }
+      }
+    }
+
+    this.cleanupLegacyWorkspaces();
+  },
+};
---
+++ floorp/browser/components/workspaces/WorkspacesWindowIdUtils.sys.mjs
@@ -0,0 +1,90 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export const EXPORTED_SYMBOLS = ["WorkspacesWindowIdUtils"];
+
+const lazy = {};
+ChromeUtils.defineESModuleGetters(lazy, {
+  WorkspacesExternalFileService:
+    "resource:///modules/WorkspacesExternalFileService.sys.mjs",
+});
+
+export const WorkspacesWindowIdUtils = {
+  get _workspacesStoreFile() {
+    return lazy.WorkspacesExternalFileService._workspacesStoreFile;
+  },
+
+  async getAllWindowAndWorkspacesData() {
+    let fileExists = await IOUtils.exists(this._workspacesStoreFile);
+    if (!fileExists) {
+      IOUtils.writeJSON(this._workspacesStoreFile, {});
+      return {};
+    }
+
+    let json = await IOUtils.readJSON(this._workspacesStoreFile);
+    return json;
+  },
+
+  async getWindowWorkspacesData(windowId) {
+    let fileExists = await IOUtils.exists(this._workspacesStoreFile);
+    if (!fileExists) {
+      let obj = {
+        windows: {},
+      };
+
+      IOUtils.writeJSON(this._workspacesStoreFile, obj);
+      return obj;
+    }
+
+    let json = await IOUtils.readJSON(this._workspacesStoreFile);
+    let result = json.windows[windowId] || {};
+    return result;
+  },
+
+  async getWindowWorkspacesDataWithoutPreferences(windowId) {
+    let workspacesData = await this.getWindowWorkspacesData(windowId);
+    delete workspacesData.preferences;
+    return workspacesData;
+  },
+
+  async getWindowWorkspacesCount(windowId) {
+    let workspacesData = await this.getWindowWorkspacesData(windowId);
+    let workspacesCount = Object.keys(workspacesData).length;
+    return workspacesCount;
+  },
+
+  async getDefaultWorkspaceId(windowId) {
+    let workspacesData = await this.getWindowWorkspacesData(windowId);
+    for (let workspaceId in workspacesData) {
+      let workspace = workspacesData[workspaceId];
+      if (workspace.defaultWorkspace) {
+        return workspaceId;
+      }
+    }
+    return null;
+  },
+
+  async getSelectedWorkspaceId(windowId) {
+    let workspacesData = await this.getWindowWorkspacesData(windowId);
+    let preferences = workspacesData.preferences || {};
+    if (preferences.selectedWorkspaceId) {
+      return preferences.selectedWorkspaceId;
+    }
+
+    let defaultWorkspaceId = await this.getDefaultWorkspaceId(windowId);
+    if (defaultWorkspaceId) {
+      return defaultWorkspaceId;
+    }
+
+    return null;
+  },
+
+  async getAllWorkspacesId(windowId) {
+    let workspacesData =
+      await this.getWindowWorkspacesDataWithoutPreferences(windowId);
+    let workspacesIds = Object.keys(workspacesData);
+    return workspacesIds;
+  },
+};
