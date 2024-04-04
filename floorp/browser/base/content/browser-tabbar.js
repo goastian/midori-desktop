0d3f14020d02f386ea27bdc4f8f7edcf52eae9e6
Ponchale <alfonso.hernandez@astian.org>
2024-04-04 02:16:36 -0500

    Use titlebar for making dragble spaces

--- floorp/browser/base/content/browser-tabbar.js
+++ floorp/browser/base/content/browser-tabbar.js
@@ -66,8 +66,9 @@ const tabbarDisplayStyleFunctions = {
             display: none !important;
           }
           #titlebar {
+            display: inherit !important;
             appearance: none !important;
-            display: none !important;
+            padding-top: 10px !important;
           }
           #TabsToolbar #workspace-button[label] > .toolbarbutton-icon,
           #TabsToolbar #firefox-view-button[flex] > .toolbarbutton-icon {
