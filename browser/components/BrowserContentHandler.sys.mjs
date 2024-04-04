85b4085481ab18009eabd37a56c1761525ed168c
Ponchale <alfonso.hernandez@astian.org>
2024-04-03 23:49:50 -0500

    fix problem with sidebar & webapps

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -959,11 +959,9 @@ function handURIToExistingBrowser(
     // Floorp Injections
   if (location == 3 || location == 0) {
   if (Services.prefs.getBoolPref("floorp.browser.sidebar2.addons.enabled")) {
-    let wins = Services.wm.getEnumerator("navigator:browser");
-    for (let win of wins) {
-      if (win.floorpSsbWindow || win.floorpWebPanel) {
-        continue;
-      }
+    var { FloorpServices } = ChromeUtils.importESModule("resource:///modules/FloorpServices.sys.mjs");
+    let win = FloorpServices.wm.getRecentWindowExcludeFloorpSpecialWindows();
+    if (win) {
 
       let browser = win.gBrowser;
       let tab = browser.addTab(uri.spec, {
@@ -971,6 +969,7 @@ function handURIToExistingBrowser(
       });
 
       win.gBrowser.selectedTab = tab;
+      win.focus();
       return;
         }
       }
--- browser/modules/URILoadingHelper.sys.mjs
+++ browser/modules/URILoadingHelper.sys.mjs
@@ -558,6 +558,14 @@ export const URILoadingHelper = {
           w.isBlankPageURL(url) &&
           !lazy.AboutNewTab.willNotifyUser;
 
+                // Floorp Injection
+        if (Services.prefs.getBoolPref("floorp.browser.sidebar2.addons.enabled")) {
+          var { FloorpServices } = ChromeUtils.importESModule("resource:///modules/FloorpServices.sys.mjs");
+          if (FloorpServices.wm.getRecentWindowExcludeFloorpSpecialWindows()) {
+            w = FloorpServices.wm.getRecentWindowExcludeFloorpSpecialWindows();
+          }
+        }
+
         let tabUsedForLoad = w.gBrowser.addTab(url, {
           referrerInfo: params.referrerInfo,
           charset,
@@ -580,6 +588,9 @@ export const URILoadingHelper = {
           globalHistoryOptions,
         });
         targetBrowser = tabUsedForLoad.linkedBrowser;
+
+        w.gBrowser.selectedTab = tabUsedForLoad;
+        // End Floorp Injection
 
         resolveOnNewTabCreated?.(targetBrowser);
         resolveOnContentBrowserCreated?.(targetBrowser);
