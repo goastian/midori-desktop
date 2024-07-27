7ec11cf73bd8bc6ca40aac14f5c746be92f50025
Ponchale <alfonso.hernandez@astian.org>
2024-07-27 13:48:34 -0500

    Bug 1904644 - Share more exception handling code. r=rhunt, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D217260

--- js/src/jit/x86/MacroAssembler-x86.cpp
+++ js/src/jit/x86/MacroAssembler-x86.cpp
@@ -21,6 +21,7 @@
 #include "vm/JitActivation.h"  // js::jit::JitActivation
 #include "vm/JSContext.h"
 #include "vm/StringType.h"
+#include "wasm/WasmStubs.h"
 
 #include "jit/MacroAssembler-inl.h"
 #include "vm/JSScript-inl.h"
@@ -563,10 +564,7 @@ void MacroAssemblerX86::handleFailureWithHandlerTail(Label* profilerExitTail,
   // If we found a catch handler, this must be a baseline frame. Restore state
   // and jump to the catch block.
   bind(&catch_);
-  loadPtr(Address(esp, ResumeFromException::offsetOfTarget()), eax);
-  loadPtr(Address(esp, ResumeFromException::offsetOfFramePointer()), ebp);
-  loadPtr(Address(esp, ResumeFromException::offsetOfStackPointer()), esp);
-  jmp(Operand(eax));
+  wasm::GenerateJumpToCatchHandler(asMasm(), esp, eax, ebx);
 
   // If we found a finally block, this must be a baseline frame. Push two
   // values expected by the finally block: the exception and BooleanValue(true).
