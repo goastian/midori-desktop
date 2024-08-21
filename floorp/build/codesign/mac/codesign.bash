81e51f36cd838d116158cf2a377a18b567954ed6
Ponchale <alfonso.hernandez@astian.org>
2024-08-21 16:03:05 -0500

    new build for Midori macOS

--- floorp/build/codesign/mac/codesign.bash
+++ floorp/build/codesign/mac/codesign.bash
@@ -127,11 +127,10 @@ codesign --force -o runtime --verbose --sign "$IDENTITY" --deep \
 codesign --force -o runtime --verbose --sign "$IDENTITY" --deep \
 "${BUNDLE}"/Contents/MacOS/updater.app
 
-# Sign floorp main exectuable
+# Sign midori main executable
 codesign --force -o runtime --verbose --sign "$IDENTITY" --deep \
 --entitlements ${BROWSER_ENTITLEMENTS_FILE} \
-"${BUNDLE}"/Contents/MacOS/floorp-bin \
-"${BUNDLE}"/Contents/MacOS/floorp
+"${BUNDLE}"/Contents/MacOS/midori
 
 # Sign Library/LaunchServices
 codesign --force -o runtime --verbose --sign "$IDENTITY" --deep \
