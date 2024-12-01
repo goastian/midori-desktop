a607c6a443ae3dbdec761d07a4b35a0a19ee3957
Ponchale <alfonso.hernandez@astian.org>
2024-12-01 14:27:41 -0500

    fix problem system scrollbar

--- widget/ScrollbarDrawingWin.cpp
+++ widget/ScrollbarDrawingWin.cpp
@@ -166,7 +166,6 @@ void ScrollbarDrawingWin::RecomputeScrollbarParams() {
   }
   ConfigureScrollbarSize(defaultSize);
 
-  if (StaticPrefs::widget_non_native_theme_win_scrollbar_use_system_size()) {
   // Do not leak system size when using ResistFingerprinting.
   if (!nsContentUtils::ShouldResistFingerprinting("No context available",
                                                   RFPTarget::CSSResolution) &&
