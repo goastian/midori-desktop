ec599dd3a22bb57afe94068b94d35f8f40a3b916
Ponchale <alfonso.hernandez@astian.org>
2024-12-04 06:29:01 -0500

    increased privacy and tracking control.

--- floorp/browser/app/profile/000-floorp.js
+++ floorp/browser/app/profile/000-floorp.js
@@ -281,6 +281,8 @@ pref("browser.urlbar.trimURLs", false);
 //プライバシー機能をオンにし、テレメトリー採取を無効化します。
 pref("privacy.trackingprotection.origin_telemetry.enabled", false, locked);
 pref("privacy.userContext.enabled", true);
+pref("privacy.donottrackheader.enabled", true);
+pref("privacy.globalprivacycontrol.enabled", true);
 pref("privacy.userContext.ui.enabled", true);
 pref("trailhead.firstrun.branches", "", locked);
 pref("extensions.webcompat-reporter.enabled", false);
