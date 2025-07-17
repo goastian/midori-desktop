f5387e93a25afbbb71e953c76f702b8d9e2fc395
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-07-17 13:48:13 -0500

    Fix addon issues when moving the profile directory

--- toolkit/mozapps/extensions/internal/XPIProvider.sys.mjs
+++ toolkit/mozapps/extensions/internal/XPIProvider.sys.mjs
@@ -486,7 +486,7 @@ class XPIState {
 
     // Builds prior to be 1512436 did not include the rootURI property.
     // If we're updating from such a build, add that property now.
-    if (!("rootURI" in this) && this.file) {
+    if (this.file) {
       this.rootURI = getURIForResourceInFile(this.file, "").spec;
     }
 
@@ -499,7 +499,10 @@ class XPIState {
       saved.currentModifiedTime != this.lastModifiedTime
     ) {
       this.lastModifiedTime = saved.currentModifiedTime;
-    } else if (saved.currentModifiedTime === null) {
+    } else if (
+      saved.currentModifiedTime === null &&
+      (!this.file || !this.file.exists())
+    ) {
       this.missing = true;
     }
   }
@@ -1469,7 +1472,7 @@ var XPIStates = {
       oldLocations.delete(loc.name);
 
       if (shouldRestoreLocationData && oldState[loc.name]) {
-        loc.restore(oldState[loc.name]);
+        changed = changed || loc.path != oldState[loc.name].path;
       }
       changed = changed || loc.changed;
 
