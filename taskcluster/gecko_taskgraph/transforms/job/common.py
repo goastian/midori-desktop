606c62752f47cf04077dc13debe2f221af8e41f9
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-09-30 21:52:59 -0500

    Bug 1944610 - Adjust windows tasks to use updated dir for hgstore. r=taskgraph-reviewers,jmaher a=release

--- taskcluster/gecko_taskgraph/transforms/job/common.py
+++ taskcluster/gecko_taskgraph/transforms/job/common.py
@@ -96,7 +96,7 @@ def support_vcs_checkout(config, job, taskdesc, sparse=False):
     if is_win:
         checkoutdir = "./build"
         geckodir = f"{checkoutdir}/src"
-        hgstore = "y:/hg-shared"
+        hgstore = "d:/hg-shared"
     elif is_docker:
         checkoutdir = "{workdir}/checkouts".format(**job["run"])
         geckodir = f"{checkoutdir}/gecko"
