e6766eefe8e45506bc71bb946a225f5d2be404f0
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-09-30 21:27:06 -0500

    Bug 1983714 - Remove now unnecessary warning from AppProvidedSearchEngine which causes tests to fail. r=mbeier a=RyanVM A new URL type has been added to the configuration. The JS Search Engine Selector that runs with FF 128 passes the new URL type to the AppProvidedSearchEngine, and hence the warning is triggered. Since ESR 128 is in maintenance mode, we can simply drop the warning.

--- toolkit/components/search/AppProvidedSearchEngine.sys.mjs
+++ toolkit/components/search/AppProvidedSearchEngine.sys.mjs
@@ -527,7 +527,6 @@ export class AppProvidedSearchEngine extends SearchEngine {
     let urlType = AppProvidedSearchEngine.URL_TYPE_MAP.get(type);
 
     if (!urlType) {
-      console.warn("unexpected engine url type.", type);
       return;
     }
 
