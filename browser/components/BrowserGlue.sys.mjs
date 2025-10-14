6a7cb8cda67d9d41a8db1a15b29b88790538ed83
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-10-14 00:15:50 -0500

    remove function dont use

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -2691,7 +2691,6 @@ BrowserGlue.prototype = {
     this._monitorHTTPSOnlyPref();
     this._monitorIonPref();
     this._monitorIonStudies();
-    this._setupSearchDetection();
 
     this._monitorGPCPref();
 
