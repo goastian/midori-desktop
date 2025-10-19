4e7e319ddef08c01cbfb37173e05dd7e42161bfb
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-10-18 23:57:41 -0500

    Bug 1994728 - Also fix dns_service_wrap test r=necko-reviewers,kershaw a=test-only

--- netwerk/test/unit_ipc/test_dns_service_wrap.js
+++ netwerk/test/unit_ipc/test_dns_service_wrap.js
@@ -2,6 +2,14 @@
 // Run test script in content process instead of chrome (xpcshell's default)
 //
 
+const overrideService = Cc[
+  "@mozilla.org/network/native-dns-override;1"
+].getService(Ci.nsINativeDNSResolverOverride);
+
+// This domain used to resolve. We fake the response to avoid
+// depending on the network for automated tests.
+overrideService.addIPOverride("xn--bcher-kva.org", "127.0.0.1");
+
 function run_test() {
   run_test_in_child("../unit/test_dns_service.js");
 }
