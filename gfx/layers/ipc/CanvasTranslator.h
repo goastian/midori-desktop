a3228b83e6502ce4241a79b4489486761c8294e8
Alfonso Hernandez <alfonso.hernandez@astian.org>
2025-10-26 04:02:34 -0500

    Force uploading recycled DataSourceSurface by CanvasTranslator r=gfx-reviewers,lsalzman As a simple workaround, use DataSourceSurfaceWrapper to force the upload of recycled DataSourceSurface.

--- gfx/layers/ipc/CanvasTranslator.h
+++ gfx/layers/ipc/CanvasTranslator.h
@@ -335,7 +335,7 @@ class CanvasTranslator final : public gfx::InlineTranslator,
   void ClearCachedResources();
 
   already_AddRefed<gfx::DataSourceSurface>
-  GetRecycledDataSurfaceForSurfaceDescriptor(TextureHost* aTextureHost);
+  MaybeRecycleDataSurfaceForSurfaceDescriptor(TextureHost* aTextureHost);
 
   const RefPtr<TaskQueue> mTranslationTaskQueue;
   const RefPtr<SharedSurfacesHolder> mSharedSurfacesHolder;
