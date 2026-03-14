cea4d6e01d0065120c246c5116048911d8df31c9
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-03-13 20:42:29 -0500

    fix problem with user-agent site compatibility netflix, Gmail etc

--- src/browser/app/profile/midori-browser.js
+++ src/browser/app/profile/midori-browser.js
@@ -11,6 +11,10 @@
 #include Memoryfox.js
 
 // Midori Browser Mods
+// Include "Firefox/x.y" in User-Agent for site compatibility (Netflix, Gmail, Mozilla Addons, etc.)
+// Result: Mozilla/5.0 (...) Gecko/20100101 Firefox/148.0 Midori/11.6.2
+pref("general.useragent.compatMode.firefox", true);
+
 pref("app.support.baseURL", "https://astian.org/community");
 pref("extensions.install_origins.enabled", true);
 pref("browser.newtabpage.activity-stream.feeds.section.highlights", true);
