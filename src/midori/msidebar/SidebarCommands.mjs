ad5840767f5fcfba31ddf4cd4e3b9bad5d8c520b
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-03-23 10:01:17 -0500

    remove old component msidebar

--- src/midori/msidebar/SidebarCommands.mjs
+++
@@ -1,7 +0,0 @@
-export function toggleSidebar(win) {
-  const enabled = Services.prefs.getBoolPref('midori.msidebar.enabled', false);
-  Services.prefs.setBoolPref('midori.msidebar.enabled', !enabled);
-  try {
-    win?.focus();
-  } catch {}
-}
