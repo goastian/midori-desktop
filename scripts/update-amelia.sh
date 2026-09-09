3adf50dd44f756cd99bc0b9e334ce77f6adffe5c
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-09-09 01:33:13 -0500

    fix problem with CI/CD

--- scripts/update-amelia.sh
+++ scripts/update-amelia.sh
@@ -3,11 +3,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+set -euo pipefail
+
 # make sure we are on root
 if [ ! -f "package.json" ]; then
   echo "Please run this script from the root of the project"
   exit 1
 fi
 
-npm update @goastian/amelia
-npm i @goastian/amelia@latest
+npm install --save-exact @goastian/amelia@latest
